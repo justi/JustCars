14b820983209d1248ea076d5a6641a483a38e8dd
Justyna Wojtczak <justine84@gmail.com>
2019-11-22 14:20:03 +0100

    Add filters to offers

--- app/resources/offer_resource.rb
+++ app/resources/offer_resource.rb
@@ -1,5 +1,12 @@
 class OfferResource < JSONAPI::Resource
   attributes :title, :content, :price
+
+  filters :title, :content, :price
+
+  # example: filter[content]=radio
+  filter :content, apply: ->(records, value, _options) {
+    records.where('offers.content LIKE :query', query: "%#{value[0]}%")
+  }
 end
 
 
